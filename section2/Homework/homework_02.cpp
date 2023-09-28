10f6934b131e0606bdebd04bb7732db95509e490
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2023-09-29 01:16:26 +0300

    complete section2

---
+++ section2/Homework/homework_02.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 1.
+	cout << "*" << endl;
+	cout << "* *" << endl;
+	cout << "* * *" << endl;
+	cout << "* * * *" << endl;
+	cout << "* * * * * *" << endl;
+
+	// 2.
+	cout << "        *        " << endl;
+	cout << "      * * *      " << endl;
+	cout << "    * * * * *    " << endl;
+	cout << "  * * * * * * *  " << endl;
+	cout << "* * * * * * * * *" << endl;
+
+	// 3.
+	cout << "    *    " << endl;
+	cout << "  * * *  " << endl;
+	cout << "* * * * *" << endl;
+	cout << "  * * *  " << endl;
+	cout << "    *    " << endl;
+
+	// 4.
+	cout << "        1        " << endl;
+	cout << "      2 3 2      " << endl;
+	cout << "    3 4 5 4 3    " << endl;
+	cout << "  4 5 6 7 6 5 4  " << endl;
+	cout << "5 6 7 8 9 8 7 6 5" << endl;
+
+	return 0;
+}
